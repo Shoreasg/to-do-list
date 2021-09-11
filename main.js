06687c085b906fb592eec375b826fa8630cda986
Wei Lun <garyganweilun@gmail.com>
2021-09-12 03:25:52 +0800

    added jqueryUI

--- main.js
+++ main.js
@@ -1,20 +1,38 @@
 const toDolist = [];
 
 const render = () => {
-    $('#to-do-list').children("ul").empty();
+    $('#to-do-list-container').children("ul").empty();
     toDolist.forEach((item) => {    
-      $('#to-do-list').children("ul").append(`<li> ${item}</li>`);
+      $('#to-do-list-container').children("ul").append(`<li class="items">${item}<span class="tooltiptext">Drag me </span></li>`);
     });
+    drag();
+  }
+
+const drag = () => {
+  $('.items').draggable({
+    containment: "#lists",
+    revert: "invalid"
+  });
+
+  
+  
+}
+
+  const submit = () =>{
+    $('form').on('submit', (e) => { //submit the things to do
+
+      const inputValue = $('#input-box').val(); //get the input
+      toDolist.push(inputValue);
+      console.log(toDolist);
+      e.preventDefault(); //prevent from resetting
+      $(e.currentTarget).trigger('reset'); // reset after entering the to do list
+      render();
+  });
   }
 
 $(() => {
-    $('form').on('submit', (e) => { //submit the things to do
 
-        const inputValue = $('#input-box').val(); //get the input
-        toDolist.push(inputValue);
-        console.log(toDolist);
-        e.preventDefault(); //prevent from resetting
-        $(e.currentTarget).trigger('reset'); // reset after entering the to do list
-        render();
-    });
+  submit();
+  
+  
 })
